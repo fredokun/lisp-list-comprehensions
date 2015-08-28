ecc1d9849d099a3a8412959173aefdd4eefd607d
Fredokun <fredokun@github.com>
2015-08-28 16:03:36 +0200

    stx highlight issue

--- makedoc.sh
+++ makedoc.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-python3 ../markdownize/markdownize.py -i ./list-comprehensions.lisp -o ./list-comprehensions.lisp.md -b '#|' -e '|#' -l "commonlisp"
+python3 ../markdownize/markdownize.py -i ./list-comprehensions.lisp -o ./list-comprehensions.lisp.md -b '#|' -e '|#' -l "lisp"
 
-pandoc -s --highlight-style=pygments list-comprehensions.lisp.md -o list-comprehensions.lisp.pdf
+# "commonlisp" for pandoc !
+#pandoc -s --highlight-style=pygments list-comprehensions.lisp.md -o list-comprehensions.lisp.pdf

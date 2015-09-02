4bd7bc011cbe982871226136af2dcd6a788accd1
Fredokun <fredokun@github.com>
2015-09-02 13:34:31 +0200

    typo in script

--- makedoc.sh
+++ makedoc.sh
@@ -7,6 +7,6 @@ python3 ../markdownize/markdownize.py -i ./list-comprehensions.lisp -o ./list-co
 # "commonlisp" for pandoc !
 pandoc -s --highlight-style=pygments list-comprehensions.lisp-pandoc.md -o list-comprehensions.lisp.pdf
 
-rm -f lisp-comprehensions.lisp-pandoc.md
+rm -f list-comprehensions.lisp-pandoc.md
 
 
